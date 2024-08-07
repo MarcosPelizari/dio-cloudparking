518b9c8f267c04a0b7025cc10166d838a9024ba3
Marcos Pelizari <m.pelizari@gmail.com>
2024-08-07 09:10:33 -0300

    First push

---
+++ src/main/java/one/digitalinnovation/parking/controller/HelloController.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.parking.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    public String hello() {
+        return "Hello DIO...";
+    }
+}
